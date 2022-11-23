2c2dae29c1e112fd22098da9d16ae07951b2b048
Matheus Farias Porto Dos Anjos <114027492+Matheusota2k@users.noreply.github.com>
2022-11-23 01:18:43 -0300

    Add files via upload

---
+++ Matheus/Projeto Canvas V2/config.php
@@ -0,0 +1,11 @@
+<?php
+
+    	$dbhost = 'Localhost';
+        $dbUsername = 'root';
+        $dbPassword = '';
+        $dbUName = 'cadastro';
+
+        $conexao = new mysqli($dbhost,$dbUsername,$dbPassword,$dbUName);
+
+
+?>
